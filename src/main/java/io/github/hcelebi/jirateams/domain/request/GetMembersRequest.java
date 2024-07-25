63f6fcc57fc8a4f7e2a6a3db4d6c3b9b8a54547f
Hakan Celebi <hakan.celebi@turk.net>
2024-07-25 14:03:34 +0300

    fix object not null mapping

--- src/main/java/io/github/hcelebi/jirateams/domain/request/GetMembersRequest.java
+++ src/main/java/io/github/hcelebi/jirateams/domain/request/GetMembersRequest.java
@@ -1,6 +1,7 @@
 package io.github.hcelebi.jirateams.domain.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.io.Serial;
@@ -12,6 +13,7 @@ public class GetMembersRequest implements Serializable {
     private static final long serialVersionUID = -2944523348051734059L;
     @JsonIgnore
     private String teamId;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String after;
     private Integer first;
 }
